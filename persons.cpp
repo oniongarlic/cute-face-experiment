6e003fc1278fd78d7e612b6ef6e62ac6c35a1424
Kaj-Michael Lang <milang@tal.org>
2025-08-15 18:29:48 +0300

    Add the persons class files

---
+++ persons.cpp
@@ -0,0 +1,160 @@
+
+#include "persons.h"
+
+using namespace std;
+
+Persons::Persons(OpenFace *of, pqxx::connection *cx)
+    : of(of), cx(cx)
+{}
+
+void Persons::save(cv::Mat vec, int faceid)
+{
+    std::string s;
+    std::string e;
+
+    e << vec;
+
+    s="INSERT INTO faces (person, embedding) VALUES ("+std::to_string(faceid)+",'"+e+"');";
+
+    pqxx::work t(*cx);
+    t.exec(s);
+    t.commit();
+}
+
+void Persons::save(cv::Mat vec)
+{
+    save(vec, current());
+}
+
+int Persons::load_embeddings()
+{
+    std::string s;
+
+    s="SELECT person,AVG(embedding) AS e FROM faces GROUP BY person";
+
+    pqxx::work t(*cx);
+
+    auto res=t.exec(s);
+
+    embeddings.clear();
+    for (const auto &row : res) {
+        std::vector<float> tmp;
+        int id=row["person"].as<int>();
+        std::string e=row["e"].as<std::string>();
+
+        cout << id << " = " << e << endl;
+
+        // remove []
+        std::stringstream se(e.substr(1, e.size()-2));
+
+        // get the numbers x,y,x,,,,
+        std::string t;
+        while (std::getline(se, t, ',')) {
+            cout << t << endl;
+            tmp.push_back(std::stof(t));
+        }
+
+        cv::Mat m(1, 128, CV_32F, tmp.data());
+
+        of->store(m, id);
+
+        m.copyTo(embeddings[id]);
+    }
+
+    t.commit();
+
+    of->train();
+
+    return embeddings.size();
+}
+
+int Persons::load_persons()
+{
+    std::string s;
+
+    s="SELECT person,name FROM persons";
+
+    pqxx::work t(*cx);
+
+    auto res=t.exec(s);
+
+    persons.clear();
+    for (const auto &row : res) {
+        int id=row["person"].as<int>();
+        std::string n=row["name"].as<std::string>();
+
+        cout << id << " = " << n << endl;
+
+        persons[id]=n;
+    }
+
+    t.commit();
+
+    cp=persons.begin();
+
+    return persons.size();
+}
+
+int Persons::load()
+{
+    int r=load_persons();
+    load_embeddings();
+
+    return r;
+}
+
+int Persons::next() {
+    if (cp!=persons.end())
+        cp++;
+    return cp->first;
+}
+
+int Persons::previous() {
+    if (cp!=persons.begin())
+        cp--;
+    return cp->first;
+}
+
+int Persons::current() {
+    return cp->first;
+}
+
+int Persons::find(int id)
+{
+    if (cp = persons.find(id); cp != persons.end()) {
+        return cp->first;
+    }
+    return -1;
+}
+
+cv::Mat Persons::current_embedding()
+{
+    return get_embedding(current());
+}
+
+std::string Persons::current_name() {
+    return cp->second;
+}
+
+cv::Mat Persons::get_embedding(int id) const
+{
+    cv::Mat e(1, 128, CV_32F);
+
+    if (auto search = embeddings.find(id); search != embeddings.end()) {
+        cout << id << search->second << endl;
+
+        e=search->second;
+    }
+
+    return e;
+}
+
+std::string Persons::get_name(int id) const
+{
+    if (auto search = persons.find(id); search != persons.end()) {
+        cout << id << search->second << endl;
+        return search->second;
+    }
+
+    return "";
+}
---
+++ persons.h
@@ -0,0 +1,42 @@
+#ifndef PERSONS_H
+#define PERSONS_H
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/ml.hpp>
+
+#include "openface.hpp"
+
+#include <pqxx/pqxx>
+
+class Persons
+{
+public:
+    Persons(OpenFace *of, pqxx::connection *cx);
+    void save(cv::Mat vec, int faceid);
+    void save(cv::Mat vec);
+
+    int load_embeddings();
+    int load_persons();
+    int load();
+
+    cv::Mat get_embedding(int id) const;
+    std::string get_name(int id) const;
+
+    int next();
+    int previous();
+    int current();
+    int find(int id);
+    std::string current_name();
+    cv::Mat current_embedding();
+private:
+    OpenFace *of;
+    pqxx::connection *cx;
+    std::map<int, cv::Mat> embeddings;
+    std::map<int, std::string> persons;
+
+    std::map<int, std::string>::iterator cp;
+};
+
+#endif // PERSONS_H
